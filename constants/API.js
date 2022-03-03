fb9d7df66890bb0a7a5f2c0971e1690764448752
Erik Peh <mephisterik01@gmail.com>
2022-03-03 19:38:38 +0800

    first commit

---
+++ constants/API.js
@@ -0,0 +1,6 @@
+export const API = "https://your_username.pythonanywhere.com";
+export const API_LOGIN = "/auth";
+export const API_SIGNUP = "/newuser";
+export const API_POSTS = "/posts";
+export const API_WHOAMI = "/whoami";
+export const API_CREATE = "/create";
