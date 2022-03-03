a881f975385d0b6452610e7454973cfd17b9dab7
Erik Peh <mephisterik01@gmail.com>
2022-03-03 22:26:38 +0800

    added api signup

--- screens/SignInSignUpScreen.js
+++ screens/SignInSignUpScreen.js
@@ -11,7 +11,7 @@ import {
   Platform,
   LayoutAnimation,
 } from "react-native";
-import { API, API_LOGIN } from "../constants/API";
+import { API, API_LOGIN, API_SIGNUP } from "../constants/API";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
@@ -28,6 +28,7 @@ export default function SignInSignUpScreen({ navigation }) {
   const [loading, setLoading] = useState(false);
   const [errorText, setErrorText] = useState("");
   const [isLogIn, setIsLogIn] = useState(true);
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   async function login() {
     console.log("---- Login time ----");
@@ -53,6 +54,34 @@ export default function SignInSignUpScreen({ navigation }) {
       setErrorText(error.response.data.description);
       if ((error.response.status = 404)) {
         setErrorText("User does not exist");
+      }
+    }
+  }
+  async function signUp() {
+    if (password != confirmPassword) {
+      setErrorText("Your passwords don't match. Check and try again.");
+    } else {
+      try {
+        setLoading(true);
+        const response = await axios.post(API + API_SIGNUP, {
+          username,
+          password,
+        });
+        console.log(response);
+        if (response.data.Error) {
+          // We have an error message for if the user already exists
+          setErrorText(response.data.Error);
+          setLoading(false);
+        } else {
+          console.log("Success signing up!");
+          setLoading(false);
+          login();
+        }
+      } catch (error) {
+        setLoading(false);
+        console.log("Error logging in!");
+        console.log(error.response);
+        setErrorText(error.response.data.description);
       }
     }
   }
@@ -97,7 +126,10 @@ export default function SignInSignUpScreen({ navigation }) {
       <View />
       <View>
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={styles.button} onPress={login}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={isLogIn ? login : signUp}
+          >
             <Text style={styles.buttonText}>
               {" "}
               {isLogIn ? "Log In" : "Sign Up"}{" "}
@@ -114,7 +146,7 @@ export default function SignInSignUpScreen({ navigation }) {
       <TouchableOpacity
         onPress={() => {
           LayoutAnimation.configureNext({
-            duration: 1500,
+            duration: 800,
             create: { type: "linear", property: "opacity" },
             update: { type: "easeInEaseOut", duration: 1000 },
           });
