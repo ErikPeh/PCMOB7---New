814148634f82a76e0a66ae25b2c28493024a7ef6
Erik Peh <mephisterik01@gmail.com>
2022-03-22 00:11:31 +0800

    first commit

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,19 +1,89 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { commonStyles, lightStyles, darkStyles } from "../styles/commonStyles";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { useSelector } from "react-redux";
+import { commonStyles, darkStyles, lightStyles } from "../styles/commonStyles";
 
-export default function EditScreen({ navigation }) {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API, API_POSTS } from "../constants/API";
+import axios from "axios";
+
+export default function EditScreen({ navigation, route }) {
+  // const styles = { ...lightStyles, ...commonStyles }
   const isDark = useSelector((state) => state.accountPrefs.isDark);
   const styles = { ...commonStyles, ...(isDark ? darkStyles : lightStyles) };
 
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    const post = route.params.post;
+    setTitle(post.title);
+    setContent(post.content);
+  }, []);
+
+  async function editPost() {
+    const post = {
+      title: title,
+      content: content,
+    };
+    const token = await AsyncStorage.getItem("token");
+    const id = route.params.post.id;
+    try {
+      console.log(token);
+      const response = await axios.put(API + API_POSTS + "/" + id, post, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      console.log(response.data);
+      navigation.navigate("Index");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, styles.title, { marginTop: 20 }]}>
-        Edit Screen
-      </Text>
+      <View style={{ margin: 20 }}>
+        <Text style={[additionalStyles.label, styles.text]}>Enter Title:</Text>
+        <TextInput
+          style={additionalStyles.input}
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+        />
+        <Text style={[additionalStyles.label, styles.text]}>
+          Enter Content:
+        </Text>
+        <TextInput
+          style={additionalStyles.input}
+          value={content}
+          onChangeText={(text) => setContent(text)}
+        />
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 20 }]}
+          onPress={editPost}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const additionalStyles = StyleSheet.create({
+  input: {
+    fontSize: 24,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+  },
+  label: {
+    fontSize: 28,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+});
